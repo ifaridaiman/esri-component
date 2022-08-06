fb6bc5555e858bc64c3a6514316cc2d28a8d47eb
ifaridaiman <ifaridaiman@gmail.com>
2022-08-06 14:17:26 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import { NotFoundPage } from './pages/error/NotFoundPage';
+import { Sprint1 } from './pages/Sprint1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App: React.FC = () => {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Sprint1/>}></Route>
+
+        {/* Not Found Page */}
+        <Route path="*" element={<NotFoundPage/>}></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Sprint1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sprint1 = () => {
+  return (
+    <div>
+      <p className="font-bold">Sprint1</p>
+      </div>
+  )
+}
